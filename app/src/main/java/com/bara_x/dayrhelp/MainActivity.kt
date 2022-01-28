fbe8457b533ad758662804c4e6c77d0a0f24c9a9
Alex <bara_x@mail.ru>
2022-01-28 19:34:50 +0300

    добавление языковых переменных

--- app/src/main/java/com/bara_x/dayrhelp/MainActivity.kt
+++ app/src/main/java/com/bara_x/dayrhelp/MainActivity.kt
@@ -2,10 +2,22 @@ package com.bara_x.dayrhelp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
+
+    // определяем что делает кнопка в ActionBar
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) finish()
+
+        return true
+    }
+
+
+
 }
