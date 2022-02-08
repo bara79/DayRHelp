4d873e6116b4b497399faef30bf8feff0da48694
bara79 <bara_x@mail.ru>
2022-02-08 17:08:42 +0300

    добавление языковых переменных и иконок для меню

--- app/src/main/java/com/bara_x/dayrhelp/MainActivity.kt
+++ app/src/main/java/com/bara_x/dayrhelp/MainActivity.kt
@@ -2,18 +2,58 @@ package com.bara_x.dayrhelp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.system.Os.close
 import android.view.MenuItem
-import com.bara_x.dayrhelp.databinding.ActivityMainBinding
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+
+
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        navView.setNavigationItemSelectedListener {
+
+            // прослушиваем меню, какие нижимаем кнопки
+            when (it.itemId) {
+
+                R.id.medicine_drawer_menu -> Toast.makeText(
+                    applicationContext,
+                    "Нажал кнопку",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            true
+        }
 
     }
+//<- кнопка назад в экшн баре, при нажатии возврат (складывает меню)
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if (toggle.onOptionsItemSelected(item)){
+
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+
 }
