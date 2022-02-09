a4d323e5e7e2e9791685b602832effd2ad1f680a
bara79 <bara_x@mail.ru>
2022-02-09 14:50:57 +0300

    создание ресайклвью и итем

---
+++ app/src/main/java/com/bara_x/dayrhelp/ListItem.kt
@@ -0,0 +1,9 @@
+package com.bara_x.dayrhelp
+
+// клас для наполнения itemLayout
+data class ListItem (
+        var image_id:Int, // картинка
+        var titleText:String, // заголовок
+        var contentText:String // описание
+
+        )
